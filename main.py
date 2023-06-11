4bb0619e454728f0030bffa4e8d79d542ecb7cd1
Steven J Hatzakis <hatgit@users.noreply.github.com>
2023-06-11 05:47:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import yaml
 load_dotenv()
 
 # Load YAML file (removing in production)
-## with open('openapi.yaml', 'r') as yaml_file:
- ##   yaml_data = yaml.safe_load(yaml_file)
+with open('openapi.yaml', 'r') as yaml_file:
+   yaml_data = yaml.safe_load(yaml_file)
 
 # Set values from environment variables
 yaml_data['oanda']['token'] = os.getenv('OANDA_TOKEN')
@@ -28,8 +28,8 @@ yaml_data['oanda']['active_account'] = os.getenv('ACTIVE_ACCOUNT')
 yaml_data['oanda']['username'] = os.getenv('OANDA_USERNAME')
 
 # Save modified YAML file (removing in production)
-## with open('openapi.yaml', 'w') as yaml_file:
-  ##  yaml.dump(yaml_data, yaml_file)
+with open('openapi.yaml', 'w') as yaml_file:
+  yaml.dump(yaml_data, yaml_file)
 
 # Setup Flask app
 app = Flask(__name__)
